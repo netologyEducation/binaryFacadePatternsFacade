e8a3bcfb8c553ee52e97020479cbce7afa547c11
noname <fromchita@yandex.ru>
2021-07-30 14:43:09 +0300

    Решение задачи

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,14 @@
+public class  BinOps {
+
+    public String sum(String a, String b){
+        Integer binA = Integer.parseInt(a,2);
+        Integer binB = Integer.parseInt(b,2);
+        return Integer.toBinaryString(binA + binB);
+    }
+
+    public String mul(String a, String b){
+        Integer binA = Integer.parseInt(a, 2);
+        Integer binB = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(binA * binB);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("1100100", "10"));
+        System.out.println(binOps.mul("10", "1100100"));
+    }
+}
